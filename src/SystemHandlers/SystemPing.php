b175925b573c33e85a38581df8e1a2a5b67fc76d
Alex O <falkonseo@gmail.com>
2024-03-13 13:48:17 +0300

    fix: add timeout to system ping

--- src/SystemHandlers/SystemPing.php
+++ src/SystemHandlers/SystemPing.php
@@ -34,6 +34,7 @@ class SystemPing
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         print_r(curl_exec($ch));
